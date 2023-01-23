e0a2dd01a5fa7c89d9bfc371a01c8d1c9922f91a
Kyle <k.james.close@gmail.com>
2023-01-22 17:17:24 -0800

    Added buttons that play the game. UI ugly at this point.

--- script.js
+++ script.js
@@ -1,77 +1,79 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll(".btn");
+const resultContainer = document.querySelector(".result");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function (event) {
+    result = play(getPlayerChoice(event), getComputerChoice());
+    checkForWinner(result);
+  });
+});
+
+
+
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3); // Generate random number between 0 - 2
-    switch(choice){
-        case 0:
-            return "ROCK";
-        case 1:
-            return "PAPER";
-        case 2:
-            return "SCISSORS";
-    }
+  let choice = Math.floor(Math.random() * 3); // Generate random number between 0 - 2
+  switch (choice) {
+    case 0:
+      return "ROCK";
+    case 1:
+      return "PAPER";
+    case 2:
+      return "SCISSORS";
+  }
 }
 
-function getPlayerChoice(){
-    let choice = prompt("Enter selection (Rock, Paper, Scissors)");
-    return choice.toUpperCase();
+function getPlayerChoice(event) {
+  return event.target.id.toUpperCase();
 }
 
-function play(playerSelection, computerSelection){
-    let loseString = `You lose. Computers ${computerSelection} beats your ${playerSelection}`;
-    let winString = `You win! Your ${playerSelection} beats the computers ${computerSelection}`;
-    let drawString = `Draw. You both chose ${playerSelection}`;
+function play(playerSelection, computerSelection) {
+  let loseString = `You lose. Computers ${computerSelection} beats your ${playerSelection}`;
+  let winString = `You win! Your ${playerSelection} beats the computers ${computerSelection}`;
+  let drawString = `Draw. You both chose ${playerSelection}`;
 
-    if(playerSelection == computerSelection){
-        return drawString;
+  if (playerSelection == computerSelection) {
+    return drawString;
+  } else if (playerSelection == "ROCK") {
+    if (computerSelection == "PAPER") {
+      return loseString;
+    } else {
+      return winString;
     }
-
-    else if(playerSelection == "ROCK"){
-        if(computerSelection == "PAPER"){
-            return loseString;
-        }    
-        else{
-            return winString;
-        }
+  } else if (playerSelection == "PAPER") {
+    if (computerSelection == "ROCK") {
+      return winString;
+    } else {
+      return loseString;
     }
-    else if(playerSelection == "PAPER"){
-        if(computerSelection == "ROCK"){
-            return winString;
-        }    
-        else{
-            return loseString;
-        }
+  } else if (playerSelection == "SCISSORS") {
+    if (computerSelection == "ROCK") {
+      return loseString;
+    } else {
+      return winString;
     }
-    else if(playerSelection == "SCISSORS"){
-        if(computerSelection == "ROCK"){
-            return loseString;
-        }    
-        else{
-            return winString;
-        }
-    }
+  }
 }
 
-function game(){
-    console.log("Welcome to Rock Paper Scissors! This is a best of 5 match");
-    let playerScore = 0;
-    let computerScore = 0;
-
-    while(playerScore < 3 && computerScore < 3){
-        let result = play(getPlayerChoice(), getComputerChoice());
-        console.log(result);
-
-        if(result.startsWith("You win")){
-            playerScore++;
-        }
-        else if(result.startsWith("You lose")){
-            computerScore++;
-        }
-    }
-    if(playerScore == 3){
-        console.log(`Congrats! You won ${playerScore} to ${computerScore}.`);
-    }
-    else{
-        console.log(`You lost. Computer won ${computerScore} to ${playerScore}.`);
-    }
+function resetScores(){
+	playerScore = 0;
+	computerScore = 0;
 }
 
-game();
+function checkForWinner(result) {
+  if (result.startsWith("You win")) {
+    playerScore++;
+  } else if (result.startsWith("You lose")) {
+    computerScore++;
+  }
+
+  if (playerScore == 5) {
+		resultContainer.textContent = 'You win!';
+		resetScores();
+  } else if (computerScore == 5) {
+		resultContainer.textContent = 'Computers wins. Better luck next time!';
+		resetScores();
+  }
+}
